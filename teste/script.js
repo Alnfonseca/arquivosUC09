5168b3223c72506f566babf419163d62f62e4695
Alnfonseca <afonsecadesantana@gmail.com>
2023-08-30 10:26:17 -0300

    UC09

--- teste/script.js
+++ teste/script.js
@@ -1,13 +1,15 @@
 let usuario = "Ana";
 let tarefas = ["Estudar para o exame"];
-tarefas.push("Fazer Compas");
+tarefas.push("Fazer Compras");
 console.log(tarefas);
 
 let usuario2 = "João";
 
 let statusDaTarefa = prompt();
 if(statusDaTarefa == "concluida"){
-    console.log(tarefas.splice(1, 0)) // esta tirando o segundo dado da lista
+    console.log(tarefas.splice([1])) // esta tirando o segundo dado da lista
 }
 
 let usuario3 = "Carlos";
+tarefas[0] = "Fazer compras"
+console.log(tarefas);
